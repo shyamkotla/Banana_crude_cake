7219a0ed4e1a6d027ed93a8a3864c521a307d9c9
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2022-05-30 23:32:49 +0530

    ground check

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,15 +7,19 @@ public class player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]    
     private float _Jumpspeed = 1f;
-    public float horizontalInput;
-    public float verticalInput;
-    public float jumpInput;
-
+    private float horizontalInput;
+    private float verticalInput;
+    [SerializeField] Transform grounpos;
+    [SerializeField] bool grounded;
+    [SerializeField] float raylength =0.1f;
     public Rigidbody2D rb;
+
+    [SerializeField] LayerMask groundlayer;
 
 
     void Start()
     {
+        grounded = false;
         //transform.position = new Vector3(0, 0, 0);
         rb = GetComponent<Rigidbody2D>();
     }
@@ -23,19 +27,53 @@ public class player : MonoBehaviour
     
     void Update()
     {
+        if(groundcheck())
+        {
+            movement();
+            jump();
+        }
+        
+    }
+
+    private bool groundcheck()
+    {
+        if (Physics2D.Raycast(grounpos.position, Vector2.down * raylength, groundlayer))
+        {
+            grounded = true;
+            Debug.Log(Physics2D.Raycast(grounpos.position, Vector2.down * raylength, groundlayer).collider.name);
+            return true;
+        }
+        else
+        {
+            grounded = false;
+            return false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(grounpos.position, Vector2.down * raylength);
+    }
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * _Jumpspeed, ForceMode2D.Impulse);
+        }
+    }
+    //private void OnDrawGizmos()
+    //{
+    //    Debug.DrawRay(transform.position, new Vector3(0f, raylength, 0f));
+    //}
+
+    private void movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         //float verticalInput = Input.GetAxis("Vertical");
-        float jumpInput = Input.GetAxis("Jump");
 
         //new Vector3(1,0,0) * 0 * 3.5f * Real time
         transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
 
         //new Vector3(0, 0, 1) * 0 * 3.5f * Real time
         //transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * _Jumpspeed, ForceMode2D.Impulse);
-        }
     }
 }
