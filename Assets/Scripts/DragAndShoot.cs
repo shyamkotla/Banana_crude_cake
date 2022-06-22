1b65fe88fc783a71b50249287ea9d3b71c4e4404
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2022-06-22 11:43:55 +0530

    Drag shoot bug fix and line renderer

--- Assets/Scripts/DragAndShoot.cs
+++ Assets/Scripts/DragAndShoot.cs
@@ -10,39 +10,51 @@ public class DragAndShoot : MonoBehaviour
     [SerializeField] private float force;
     [SerializeField] Transform aimer;
     [SerializeField] Transform dragger;
+    [SerializeField] LineRenderer lr;
     Rigidbody2D rb;
     Vector2 dir;
     Camera camRef;
+    float angle;
 
     // Start is called before the first frame update
     void Start()
     {
+        lr = GetComponent<LineRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        camRef = Camera.main;        
+        camRef = Camera.main;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     
     private void OnMouseDown()
     {
+        
         aimer.gameObject.SetActive(true);
         dragger.gameObject.SetActive(true);
         Debug.Log("started");
-        
+        lr.SetPosition(0, transform.position);
+
     }
     private void OnMouseDrag()
     {
+        lr.enabled = true;
         dragPos = camRef.ScreenToWorldPoint(Input.mousePosition);
         dragger.position = dragPos; // B vector
         startPos = transform.position; // A vector
 
+        lr.SetPosition(1, aimer.position);// linerenderer
+
         dir = dragPos - startPos; // dir vector
 
         aimer.position = startPos - dir;// mirror of dir vector
+
+        angle = Vector2.Angle(aimer.position.normalized, Vector2.right);
+        Debug.Log(angle);
     }
     private void OnMouseUp()
     {
@@ -56,8 +68,16 @@ public class DragAndShoot : MonoBehaviour
         var newdirection = startPos - dir;
         aimer.position = newdirection;
         newdirection.Normalize();
-        rb.AddForce(newdirection*force, ForceMode2D.Impulse);
+
+
+        var forcedir = aimer.position - transform.position;
+       
         
+
+       rb.AddForce(forcedir/**force*/, ForceMode2D.Impulse);
+        
+        lr.enabled = false;
+        //Debug.Log(aimer.position);
 
     }
 }
